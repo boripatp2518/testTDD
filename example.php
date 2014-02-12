ae7ca8cd7cf65ad0a10a3cf9a7fa7133c637c0c8
Boripat Petchkaew <boripat2518@github.com>
2014-02-12 10:58:47 +0700

    first

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+$txtInput=$argv[1];
+$ops = $
+$txtArray=explode(',',$txtInput);
+$open =$txtInput[0];
+$close = $txtInput[strlen($txtInput)-1];
+$firstNumber =substr($txtArray[0],1,strlen(txtArray[0])-1);
+$secondeNumber =  
+?>
